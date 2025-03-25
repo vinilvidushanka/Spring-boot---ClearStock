7fd4e30be9e0f6e32187f2a365106fcd63d364e6
vinilvidu <vidushankav@gmail.com>
2025-03-25 20:06:15 +0530

    image uploading process

--- BackEnd/src/main/java/com/example/backend/config/WebSecurityConfig.java
+++ BackEnd/src/main/java/com/example/backend/config/WebSecurityConfig.java
@@ -1,3 +1,77 @@
+//package com.example.backend.config;
+//
+//import com.example.backend.service.impl.UserServiceImpl;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//@EnableWebSecurity
+//@Configuration
+//@EnableMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfig {
+//    @Autowired
+//    private UserServiceImpl userService;
+//    @Autowired
+//    private JwtFilter jwtFilter;
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+//    }
+//    @Bean
+//    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+//            throws Exception {
+//        return authenticationConfiguration.getAuthenticationManager();
+//    }
+//    @Bean
+//    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers(
+//                                "/api/v1/auth/authenticate",
+//                                "/api/v1/user/save",
+//                                "/api/v1/auth/refreshToken",
+//                                "/api/v1/user/delete/{id}",
+//                                "/api/v1/user/update/{id}",
+//                                "/api/v1/user/getAll",
+//                                "/api/v1/category/save",
+//                                "/api/v1/category/delete/{id}",
+//                                "/api/v1/category/update/{id}",
+//                                "/api/v1/category/getAll",
+//                                "/api/v1/store/save",
+//                                "/api/v1/store/delete/{id}",
+//                                "/api/v1/store/update/{id}",
+//                                "/api/v1/store/getAll",
+//                                "/uploads/**",
+//                                "/v3/api-docs/**",
+//                                "/swagger-ui/**",
+//                                "/swagger-ui.html").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//
+//    }
+//
+//}
+
 package com.example.backend.config;
 
 import com.example.backend.service.impl.UserServiceImpl;
@@ -25,6 +99,7 @@ public class WebSecurityConfig {
     private UserServiceImpl userService;
     @Autowired
     private JwtFilter jwtFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -33,11 +108,13 @@ public class WebSecurityConfig {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
+
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
@@ -58,6 +135,7 @@ public class WebSecurityConfig {
                                 "/api/v1/store/delete/{id}",
                                 "/api/v1/store/update/{id}",
                                 "/api/v1/store/getAll",
+                                "/uploads/**", // Allow access to uploads directory
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html").permitAll()
